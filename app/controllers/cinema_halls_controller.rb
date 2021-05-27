b66b52fece73e23782c20ad164a15b33bd2b3fd7
Anna Kotowicz <annakodowicz@gmail.com>
2021-05-27 15:39:01 +0200

    refactoring of a controller

--- app/controllers/cinema_halls_controller.rb
+++ app/controllers/cinema_halls_controller.rb
@@ -1,38 +1,34 @@
 # frozen_string_literal: true
 
 class CinemaHallsController < ApplicationController
-  before_action :set_cinema_hall, only: %i[show update destroy]
-
   # GET /cinema_halls
   def index
-    # return only available cinema_halls? or add additional attribute
-    # cinema_hall available? if there's no screening at pointed time range
-    @cinema_halls = CinemaHall.all.map do |hall|
+    cinema_halls = CinemaHall.all.map do |hall|
       render_cinema_hall(hall)
     end
-    render json: @cinema_halls
+    render json: cinema_halls
   end
 
   # GET /cinema_halls/:id
   def show
-    render json: @cinema_hall
+    render json: render_cinema_hall(cinema_hall)
   end
 
   # POST /cinema_halls/
   def create
-    @cinema_hall = CinemaHall.create(cinema_hall_params)
+    cinema_call = CinemaHall.create(cinema_hall_params)
 
-    if @cinema_hall.save
-      render json: @cinema_hall, status: :created
+    if cinema_hall.valid?
+      render json: render_cinema_hall(cinema_hall), status: :created
     else
-      render json: @cinema_hall.errors, status: :unprocessable_entity
+      render json: cinema_hall.errors, status: :unprocessable_entity
     end
   end
 
   # PUT /cinema_halls/:id
   def update
-    if @cinema_hall.update(cinema_hall_params)
-      render json: @cinema_hall, status: :ok
+    if cinema_hall.update(cinema_hall_params)
+      render json: render_cinema_hall(@cinema_hall), status: :ok
     else
       render json: @cinema_hall.errors, status: :unprocessable_entity
     end
@@ -40,10 +36,10 @@ class CinemaHallsController < ApplicationController
 
   # DELETE /cinema_halls/:id
   def destroy
-    if @cinema_hall.destroy
-      render json: @cinema_hall, status: :ok
+    if cinema_hall.destroy
+      render json: render_cinema_hall(cinema_hall), status: :ok
     else
-      render json: @cinema_hall.errors, status: :unprocessable_entity
+      render json: cinema_hall.errors, status: :unprocessable_entity
     end
   end
 
@@ -57,11 +53,11 @@ class CinemaHallsController < ApplicationController
     }
   end
 
-  def set_cinema_hall
-    @cinema_hall = CinemaHall.find(params[:id])
+  def cinema_hall
+    cinema_hall = CinemaHall.find(params[:id])
   end
 
   def cinema_hall_params
-    params.require(:cinema_hall).permit(:capacity, :name, :starts_at, :ends_at)
+    params.require(:cinema_hall).permit(:capacity, :name)
   end
 end
