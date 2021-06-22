cfbb1b1cd82f1357d1674e7070a9ea875133d253
Anna Kotowicz <annakodowicz@gmail.com>
2021-06-22 21:16:35 +0200

    use cases tests

---
+++ spec/concepts/reservations/use_cases/create_online_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Reservations::UseCases::CreateOnline do
+  describe '.call' do
+    let(:user) { create :user }
+    let(:screening) { create :screening }
+    let(:params) do
+      {
+        screening_id: screening.id,
+        tickets: [
+          { seat: 'A1', price: 15, ticket_type: 'normal' }
+        ]
+      }
+    end
+    let(:instance) { described_class.new(params: params, user: user) }
+
+    it 'creates reservation' do
+      expect { instance.call }.to change { Reservation.count }.by(1)
+    end
+
+    it 'returns reservation' do
+      expect(instance.call).to be_a_kind_of(Reservation)
+    end
+  end
+end
---
+++ spec/concepts/users/repository_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Users::Repository do
+  describe '.offline_user' do
+    let!(:fake_user) { create :fake_user }
+    let(:instance) { described_class.new }
+
+    it 'returns fake user' do
+      expect(instance.offline_user).to eq(fake_user)
+    end
+  end
+end
